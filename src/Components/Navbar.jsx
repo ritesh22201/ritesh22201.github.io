65ef9ac6e77150b5a9a79af76421aa7780bd0f37
ritesh22201 <riteshgoswami22201@gmail.com>
2023-06-12 23:55:44 +0530

    code added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,23 @@
 import { Box, Button, Link, Flex, Divider } from '@chakra-ui/react'
 import React, { useEffect } from 'react';
 import { HashLink } from 'react-router-hash-link';
+import Ritesh_Goswami_Resume from '../Assets/Ritesh-Goswami-Resume.pdf';
 import './Button.css';
 import 'animate.css';
 import AOS from 'aos';
 
 const Navbar = () => {
 
-    const resumeHandler = () => {
-        window.open('https://drive.google.com/file/d/12vmGRzVMprilVwXAke-UsZBhbs1Q3ldT/view');
-    }
+    // const resumeHandler = () => {
+    //     window.open('https://drive.google.com/file/d/12vmGRzVMprilVwXAke-UsZBhbs1Q3ldT/view');
+    // }
 
     useEffect(() => {
         AOS.init({ delay: 300 });
     }, [])
 
     return (
-        <Box position="sticky" top='0' className={'bg'} color={'white'} p={'12px 0'} fontSize={'19px'}>
+        <Box boxShadow= 'rgba(0, 0, 0, 0.24) 0px 3px 8px' position="sticky" top='0' className={'bg'} color={'white'} p={'12px 0'} fontSize={'19px'}>
             <nav id='nav-menu' className='nav-menu'>
                 <Flex m='0 30px' p={'10px'} alignItems={'center'} justifyContent={'space-between'}>
                     <Box className='nav-logo' fontSize='25px' textAlign='start' w={'20%'}>
@@ -29,12 +30,12 @@ const Navbar = () => {
                             <HashLink className='nav-link skills' style={{ textDecoration: "none" }} to='/#skills'>Skills</HashLink>
                             <HashLink className='nav-link projects' style={{ textDecoration: "none" }} to='/#projects'>Projects</HashLink>
                             <HashLink className='nav-link contact' style={{ textDecoration: "none" }} to='/#contact'>Contact</HashLink>
-                            <Button p={'7px 17px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} onClick={resumeHandler} id='resume-button-1' className='nav-link resume'>Resume</Button>
-                            {/* <Link fontWeight={'bold'} borderRadius={'5px'} p={'7px 17px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} id='resume-button-2' href={Ritesh_Goswami_Resume} download={true} target='_blank'> */}
-                                {/* <Button>
+                            {/* <Button p={'7px 17px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} onClick={resumeHandler} id='resume-button-1' className='nav-link resume'>Resume</Button> */}
+                            <Link fontWeight={'bold'} borderRadius={'5px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} className='nav-link resume' id='resume-button-1' href={Ritesh_Goswami_Resume} download={true} target='_blank'> 
+                                <Button backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} _hover={'none'}>
                                     Resume
-                                </Button> */}
-                            {/* </Link> */}
+                                </Button>
+                            </Link>
                         </Flex>
                     </Box>
                 </Flex>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,13 +10,18 @@ const Home = () => {
     <Box p={'60px 0 40px 0'} id='home' color='white' bg={'black'} >
       <Flex justifyContent={'space-around'} alignItems={'center'}>
         <Box>
-            <Heading color={'gray.300'} mb={'20px'} fontSize={'25px'}>Hi, I'M</Heading>
-            <Heading id='user-detail-name' bgGradient={'linear-gradient(to right, #01b395, #03bd69)'} bgClip='text' mb={'20px'} fontSize={'50px'}>RITESH GOSWAMI</Heading>
-            <Heading color={'gray.300'} mb={'40px'} fontSize={'25px'}>FULL STACK WEB DEVELOPER</Heading>
-            <Link fontWeight={'bold'} borderRadius={'5px'} p={'7px 17px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} id='resume-button-2' href={Ritesh_Goswami_Resume} download={true} target='_blank'>Download Resume</Link>
+          <Heading color={'gray.300'} mb={'20px'} fontSize={'25px'}>Hi, I'M</Heading>
+          <Heading id='user-detail-name' bgGradient={'linear-gradient(to right, #01b395, #03bd69)'} bgClip='text' mb={'20px'} fontSize={'50px'}>RITESH GOSWAMI</Heading>
+          <Heading color={'gray.300'} mb={'40px'} fontSize={'25px'}>FULL STACK WEB DEVELOPER</Heading>
+          {/* <Link fontWeight={'bold'} borderRadius={'5px'} p={'7px 17px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} id='resume-button-2' href={Ritesh_Goswami_Resume} download={true} target='_blank'>Download Resume</Link> */}
+          <Link fontWeight={'bold'} borderRadius={'5px'} backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} fontFamily={'cursive'} id='resume-button-2' href={Ritesh_Goswami_Resume} download={true} target='_blank'>
+            <Button backgroundImage={'linear-gradient(to right, #01b395, #03bd69)'} _hover={'none'}>
+              Download Resume
+            </Button>
+          </Link>
         </Box>
         <Box>
-          <Image w={'350px'} borderRadius={'50%'} className='home-img' src={image} alt='profile image'/>
+          <Image w={'350px'} borderRadius={'50%'} className='home-img' src={image} alt='profile image' />
         </Box>
       </Flex>
     </Box>
